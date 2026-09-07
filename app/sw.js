afeaf0719d5d079b5936317524d78f6dd7fa4655
hadysayed <hadysayed3369@gmail.com>
2026-09-07 03:29:47 +0300

    release: صفحة هبوط احترافية عالمية + أيقونات PWA حقيقية(تحل مشكلة أيقونة الكروم) + سعر 49→27 + QR code

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'vguard-v2';
-const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e){
   e.waitUntil(
